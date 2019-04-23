da0d333431a3e68ea3f68ea882e9fdd8ab26b64e
Eetu <eetu@irvees.com>
2019-04-23 13:11:09 +0800

    ^ fix bug

--- app/src/main/java/com/l/zhuoyaohook/Hook.java
+++ app/src/main/java/com/l/zhuoyaohook/Hook.java
@@ -2,68 +2,43 @@ package com.l.zhuoyaohook;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Environment;
-import android.text.TextUtils;
-
-import java.io.File;
-import java.io.FileInputStream;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
 
-    int multi = 2;
-
+    private final XSharedPreferences sp;
 
     public Hook() {
-        File file = new File(Environment.getExternalStorageDirectory() + "/zhuoyaohookconfig.txt");
-        String string = readString(file.getAbsolutePath(), "utf-8");
-        if (!TextUtils.isEmpty(string)) {
-            try {
-                Integer integer = Integer.valueOf(string);
-                int i = integer.intValue();
-                multi = i;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static String readString(String file, String charset) {
-        byte[] data = readBytes(file);
-        String ret = null;
-
-        try {
-            ret = new String(data, charset);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return ret;
-    }
-
-    public static byte[] readBytes(String file) {
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            int len = fis.available();
-            byte[] buffer = new byte[len];
-            fis.read(buffer);
-            fis.close();
-            return buffer;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
-
+        sp = new XSharedPreferences(BuildConfig.APPLICATION_ID);
+        sp.makeWorldReadable();
     }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.contains("com.tencent.gwgo")) {
+
+            XposedBridge.log("hook sensor");
+            Class<?> sensorEventQueue_Class = Class.forName("android.hardware.SystemSensorManager$SensorEventQueue");
+            if (sensorEventQueue_Class != null) {
+                XposedBridge.hookAllMethods(sensorEventQueue_Class, "dispatchSensorEvent", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        boolean isSwitch = sp.getBoolean("isSwitch", false);
+                        if (isSwitch) {
+                            int multi = sp.getInt("num", 2);
+                            ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * multi;
+                        }
+                        super.beforeHookedMethod(param);
+                    }
+                });
+            }
+
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -72,18 +47,6 @@ public class Hook implements IXposedHookLoadPackage {
                     XposedBridge.log("find zhuoyao");
 
                     ClassLoader classLoader = ((Context) param.args[0]).getClassLoader();
-
-                    Class<?> sensorEventQueue_Class = Class.forName("android.hardware.SystemSensorManager$SensorEventQueue");
-                    if (sensorEventQueue_Class != null) {
-                        XposedBridge.hookAllMethods(sensorEventQueue_Class, "dispatchSensorEvent", new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-
-                                ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * multi;
-                                super.beforeHookedMethod(param);
-                            }
-                        });
-                    }
 
                     //vm fake camera vbox
                     Class<?> info_b_Class = classLoader.loadClass("com.tencent.bugly.msdk.crashreport.common.info.b");
@@ -157,7 +120,7 @@ public class Hook implements IXposedHookLoadPackage {
 
                     Class<?> info_a_Class = classLoader.loadClass("com.tencent.bugly.msdk.crashreport.common.info.a");
                     if (info_a_Class != null) {
-                        XposedHelpers.findAndHookMethod(info_a_Class, "R", Context.class, new XC_MethodHook() {
+                        XposedHelpers.findAndHookMethod(info_a_Class, "R", new XC_MethodHook() {
                             @Override
                             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                                 super.afterHookedMethod(param);
--- app/src/main/java/com/l/zhuoyaohook/MainActivity.java
+++ app/src/main/java/com/l/zhuoyaohook/MainActivity.java
@@ -1,7 +1,6 @@
 package com.l.zhuoyaohook;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,125 +9,15 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox cb;
-
-    /**
-     * 如果文件不存在，就创建文件
-     *
-     * @param path 文件路径
-     * @return
-     */
-    public static String createIfNotExist(String path) {
-        File file = new File(path);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return path;
-    }
-
-    /**
-     * 向文件中写入数据
-     *
-     * @param filePath 目标文件全路径
-     * @param data     要写入的数据
-     * @return true表示写入成功  false表示写入失败
-     */
-    public static boolean writeBytes(String filePath, byte[] data) {
-        try {
-            FileOutputStream fos = new FileOutputStream(filePath);
-            fos.write(data);
-            fos.close();
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
-
-    /**
-     * 从文件中读取数据
-     *
-     * @param file
-     * @return
-     */
-    public static byte[] readBytes(String file) {
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            int len = fis.available();
-            byte[] buffer = new byte[len];
-            fis.read(buffer);
-            fis.close();
-            return buffer;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
-
-    }
-
-    /**
-     * 向文件中写入字符串String类型的内容
-     *
-     * @param file    文件路径
-     * @param content 文件内容
-     * @param charset 写入时候所使用的字符集
-     */
-    public static void writeString(String file, String content, String charset) {
-        try {
-            byte[] data = content.getBytes(charset);
-            writeBytes(file, data);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    /**
-     * 从文件中读取数据，返回类型是字符串String类型
-     *
-     * @param file    文件路径
-     * @param charset 读取文件时使用的字符集，如utf-8、GBK等
-     * @return
-     */
-    public static String readString(String file, String charset) {
-        byte[] data = readBytes(file);
-        String ret = null;
-
-        try {
-            ret = new String(data, charset);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return ret;
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final File file = new File(Environment.getExternalStorageDirectory() + "/zhuoyaohookconfig.txt");
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        writeString(file.getAbsolutePath(), SPUtils.getNum() + "", "utf-8");
 
         cb = findViewById(R.id.cb);
         cb.setChecked(SPUtils.isSwitch());
@@ -153,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
                         Integer integer = Integer.valueOf(numStr);
                         int i = integer.intValue();
                         SPUtils.setNumber(i);
-                        writeString(file.getAbsolutePath(), i + "", "utf-8");
                     } catch (Exception e) {
                         Toast.makeText(App.mContext, e.getMessage() + e.getCause(), Toast.LENGTH_SHORT).show();
                     }
